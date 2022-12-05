0ffbc61ea206cc79a7f614f99e2fa2ba81e55390
Dantec39 <dante.cornet@gmail.com>
2022-12-05 15:57:05 +0100

    Bugfixing in main.py
    When using the entire input.csv file the script would run into some bugs.
    
    I filtered out all the None objects to exclude running into this issue

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ urllib3.disable_warnings()
 def generate_switch_interface_list():
     st = time.time()
     result = []
+    i = 0
 
     # Read input MAC addresses
     with open("input.csv", "r") as f:
@@ -15,13 +16,15 @@ def generate_switch_interface_list():
         for line in reader:
             device_type = line[3]
             device_mac = line[6]
-            if device_type == TELEVIC_DEVICE_TYPE:
-                switch_ip, switch_interface = find_switch_for_mac(device_mac)
-                result += [{
-                    "device" : device_mac,
-                    "ip" : switch_ip,
-                    "port" : switch_interface
-                }]
+            if device_type == TELEVIC_DEVICE_TYPE and str(device_mac[0:5]) == '00:0E':
+                switch = find_switch_for_mac(device_mac)
+                if switch is not None:
+                    switch_ip, switch_interface = switch
+                    result += [{
+                        "device" : device_mac,
+                        "ip" : switch_ip,
+                        "port" : switch_interface
+                    }]
     
     # Write output switches/ports
     with open("output.csv", "w") as f:
@@ -37,17 +40,18 @@ def find_switch_for_mac(mac):
     prime_host = os.environ['PRIME_HOST']
     prime_user = os.environ['PRIME_USERNAME']
     prime_pass = os.environ['PRIME_PASSWORD']
-
     # Get client details for given MAC address
     url = f"{prime_host}/webacs/api/v4/data/Clients.json?macAddress={mac.replace(':', '')}"
     resp = requests.request("GET", "https://" + url, verify=False, auth=requests.auth.HTTPBasicAuth(prime_user, prime_pass))
-    client_url = resp.json()['queryResponse']['entityId'][0]['@url']
-    client_resp = requests.request("GET", client_url + ".json", verify=False, auth=requests.auth.HTTPBasicAuth(prime_user, prime_pass))
-    switch_det = client_resp.json()['queryResponse']['entity'][0]['clientsDTO']
-    switch_ip = switch_det['deviceName']
-    switch_interface = switch_det['clientInterface']
+    query_response = resp.json()['queryResponse']
+    if query_response['@count'] == 1:
+        client_url = query_response['entityId'][0]['@url']
+        client_resp = requests.request("GET", client_url + ".json", verify=False, auth=requests.auth.HTTPBasicAuth(prime_user, prime_pass))
+        switch_det = client_resp.json()['queryResponse']['entity'][0]['clientsDTO']
+        switch_ip = switch_det['deviceName']
+        switch_interface = switch_det['clientInterface']
 
-    return (switch_ip, switch_interface)
+        return (switch_ip, switch_interface)
 
 if __name__ == "__main__":
     load_dotenv()
