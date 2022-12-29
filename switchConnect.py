48a1c5ee64770ed0dc8db39035a744db5963689f
Dantec39 <dante.cornet@gmail.com>
2022-12-29 15:21:02 +0100

    add port status check

--- switchConnect.py
+++ switchConnect.py
@@ -38,14 +38,17 @@ def switch_login():
 def modify_port(switch, port):
     ### Modify port config when necessary
     config_list = [ "int " + port,]; config_output = None
+    
     port_power = switch.send_command("sh power in  " + port )
     if "static" not in port_power:
         # config_list += ["power in static max 30000",]
         print("power set")
+        
     port_description = switch.send_command("sh int " + port + " description")
     if "#televic_script" not in port_description:
         # config_list += ["description #televic_script",]
         print("description set")
+        
     if len(config_list) > 1:
         # config_output = switch.send_config_set(config_list)
         return config_output
@@ -55,17 +58,19 @@ def cleanup_config(switch, port_list):
         all_ports = switch.send_command("sh int desc | i #televic_script")
         ### Transform ports string into list and remove unnecessary info
         all_ports = all_ports.split(sep= None, maxsplit=-1)
+        ports_status = all_ports[1::4]
         all_ports = all_ports[0::4]
         ### Create list with all ports that still have televic config but no televic device connected
         old_ports = list(set(all_ports) - set(port_list))
         if len(old_ports) > 0:
-            for old_port in old_ports:
-                config_list = [
-                "int " + old_port,
-                "default desc",
-                "power in auto",
-                ]
-                switch.send_config_set(config_list)
+            for old_port,port_status in zip(old_ports,ports_status):
+                if port_status == "down":
+                    config_list = [
+                        "int " + old_port,
+                        "default desc",
+                        "power in auto",
+                    ]
+                    switch.send_config_set(config_list)
     return None
 
 if __name__ == "__main__":
